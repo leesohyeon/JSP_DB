e6fe675c97773c9004c107cf96c02cf209063d48
이소현 <dlthgus0702@gmail.com>
2019-09-09 10:30:28 +0900

    Add files via upload

---
+++ chapter11.sql
@@ -0,0 +1,11 @@
+drop table member;
+create table member(
+id varchar(20) not null primary key,
+passwd varchar(20) not null,
+name varchar(20) not null,
+reg_date datetime not null
+);
+
+select * from member;
+
+show tables;
